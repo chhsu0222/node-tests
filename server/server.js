9bc4cc998471a3d6f298cdbf541bf741e1a27c4f
chhsu_startek <ch@startek-eng.com>
2017-10-27 09:26:01 +0800

    testing Express application part 2 (supertest)

--- server/server.js
+++ server/server.js
@@ -3,8 +3,25 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  //res.status(200).send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
 
+// GET /users (an array of object)
+// Give users a name prop and age prop
+app.get('/users', (req, res) => {
+  res.status(200).send([{
+    name: 'CH',
+    age: 30
+  }, {
+    name: 'Garfield',
+    age: 34
+  }]);
 });
 
 app.listen(3000);
+
+module.exports.app = app;
